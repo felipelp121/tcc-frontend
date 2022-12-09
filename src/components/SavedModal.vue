f2af7e871064e32662cf4215026e62e69149d3b7
Felipe <felopesserra@id.uff.br>
2022-12-09 00:51:59 -0300

    test 3

--- src/components/SavedModal.vue
+++ src/components/SavedModal.vue
@@ -54,6 +54,7 @@ export default {
         {
           method: "POST",
           headers: {
+            Accept: "application/json",
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
